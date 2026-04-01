3ce50b8be2891775d0d1ef3b247038eee5334103
Harmanjit Singh <harmanjitsingh3002@gmail.com>
2026-04-01 17:52:07 +0530

    Update settings

--- vite.config.ts
+++ vite.config.ts
@@ -72,10 +72,10 @@ export default defineConfig(({ mode }) => {
         },
       }),
     ],
-    define: {
-      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-    },
+    // define: {
+    //   "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+    //   "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+    // },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
